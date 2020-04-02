66de11f83876fe98e66f766544494998b4904242
Eric Busboom <eric@civicknowledge.com>
2020-04-01 18:24:18 -0700

    Fix stuff

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     ],
     keywords = 'cli',
     packages = find_packages(exclude=['docs', 'tests*']),
-    install_requires = ['yaml', 'pandas', 'gspread','df2gspread'],
+    install_requires = ['pyyaml', 'pandas', 'gspread','df2gspread'],
     extras_require = {
         'test': ['coverage', 'pytest', 'pytest-cov'],
     },
