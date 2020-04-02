b8b9b8271778168b504df036de09f6ecdd5292c7
Eric Busboom <eric@civicknowledge.com>
2020-04-01 20:24:42 -0700

    Missed an requirement

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     ],
     keywords = 'cli',
     packages = find_packages(exclude=['docs', 'tests*']),
-    install_requires = ['pyyaml', 'pandas', 'gspread','df2gspread', 'gspread_dataframe d'],
+    install_requires = ['pyyaml', 'pandas', 'gspread','df2gspread', 'gspread_dataframe'],
     extras_require = {
         'test': ['coverage', 'pytest', 'pytest-cov'],
     },
